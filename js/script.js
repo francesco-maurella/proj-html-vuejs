10647b07c3a8b2b9c3f0a6b379bfbc02f3b14eeb
Francesco Maurella <francesco.maurella@live.it>
2021-02-22 14:01:58 +0100

    Inizio utilizzo dati VueJS (topMenu)

---
+++ js/script.js
@@ -0,0 +1,16 @@
+new Vue ({
+  // ELEMENT
+  el: '#app',
+
+  //DATA
+  data: {
+    topMenu: [
+      'Home',
+      'Pages',
+      'Courses',
+      'Features',
+      'Blog',
+      'Shop',
+    ]
+  }
+})
